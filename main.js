8fbed22cecbd53b268c5746bc681b7c6dca68a67
Mohammad sami <113047563+shaikh-sam@users.noreply.github.com>
2023-07-13 17:38:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,119 @@
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+const resizeImg = require('resize-img');
+const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron');
+const isDev = process.env.NODE_ENV !== 'production';
+const isMac = process.platform === 'darwin';
+
+let mainWindow;
+let aboutWindow;
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    width: isDev ? 1000 : 500,
+    height: 600,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+   mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+}
+
+function createAboutWindow() {
+  aboutWindow = new BrowserWindow({
+    width: 300,
+    height: 300,
+    title: 'About Electron',
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+  });
+
+   aboutWindow.loadFile(path.join(__dirname, './renderer/about.html'));
+}
+app.on('ready', () => {
+  createMainWindow();
+
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+  mainWindow.on('closed', () => (mainWindow = null));
+});
+
+const menu = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  {
+    role: 'fileMenu',
+  },
+  ...(!isMac
+    ? [
+        {
+          label: 'Help',
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+
+  ...(isDev
+    ? [
+        {
+          label: 'Developer',
+          submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { type: 'separator' },
+            { role: 'toggledevtools' },
+          ],
+        },
+      ]
+    : []),
+];
+
+ipcMain.on('image:resize', (e, options) => {
+  options.dest = path.join(os.homedir(), 'imageresizer');
+  resizeImage(options);
+});
+
+async function resizeImage({ imgPath, height, width, dest }) {
+  try {
+    const newPath = await resizeImg(fs.readFileSync(imgPath), {
+      width: +width,
+      height: +height,
+    });
+    const filename = path.basename(imgPath);
+    if (!fs.existsSync(dest)) {
+      fs.mkdirSync(dest);
+    }
+    fs.writeFileSync(path.join(dest, filename), newPath);
+    mainWindow.webContents.send('image:done');
+    shell.openPath(dest);
+  } catch (err) {
+    console.log(err);
+  }
+}
+app.on('window-all-closed', () => {
+  if (!isMac) app.quit();
+});
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
+});
